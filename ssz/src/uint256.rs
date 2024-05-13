28d7ccd462352e505a527bc4e7c6048234ff3ffb
Tumas <tumas@grandine.io>
2024-05-13 18:11:25 +0300

    Make get_custody_columns tests pass

--- ssz/src/uint256.rs
+++ ssz/src/uint256.rs
@@ -271,7 +271,7 @@ impl Uint256 {
         Self(self.into_raw().saturating_mul(rhs.into_raw()))
     }
 
-    const fn into_raw(self) -> RawUint256 {
+    pub const fn into_raw(self) -> RawUint256 {
         self.0
     }
 }
