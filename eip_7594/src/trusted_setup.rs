051f6b8a4d88a36e34b87bec199f6bd364399c95
hangleang <hangleang127@gmail.com>
2024-07-17 12:47:12 +0700

    update c-kzg & change to ethereum_settings

--- eip_7594/src/trusted_setup.rs
+++ eip_7594/src/trusted_setup.rs
@@ -1,25 +1,5 @@
-use std::{
-    path::Path,
-    sync::OnceLock
-};
-
-use anyhow::{anyhow, Result};
-use c_kzg::KzgSettings;
+use c_kzg::{KzgSettings, ethereum_kzg_settings};
 
 pub fn settings() -> &'static KzgSettings {
-    static KZG_SETTINGS: OnceLock<KzgSettings> = OnceLock::new();
-
-    KZG_SETTINGS.get_or_init(|| match load_kzg_settings() {
-        Ok(settings) => settings,
-        Err(error) => {
-            panic!("failed to load kzg trusted setup: {error:?}");
-        }
-    })
-}
-
-fn load_kzg_settings() -> Result<KzgSettings> {
-    let trusted_setup_file = Path::new("kzg_utils/src/trusted_setup.txt");
-    println!("{:?}", trusted_setup_file);
-    assert!(trusted_setup_file.exists());
-    KzgSettings::load_trusted_setup_file(trusted_setup_file, 8).map_err(|error| anyhow!(error))
+    ethereum_kzg_settings(0)
 }
