16bf5c1f858a9a0326f5b752281c4c628cb40358
hangleang <hangleang127@gmail.com>
2024-12-11 19:10:46 +0700

    Remove unused consolidation request root from post electra body trait

--- types/src/traits.rs
+++ types/src/traits.rs
@@ -1257,7 +1257,6 @@ pub trait PostElectraBeaconBlockBody<P: Preset>: PostDenebBeaconBlockBody<P> {
     fn attester_slashings(
         &self,
     ) -> &ContiguousList<ElectraAttesterSlashing<P>, P::MaxAttesterSlashingsElectra>;
-    fn consolidation_requests_root(&self) -> H256;
     fn execution_requests(&self) -> &ExecutionRequests<P>;
 }
 
@@ -1270,10 +1269,6 @@ impl<P: Preset> PostElectraBeaconBlockBody<P> for ElectraBeaconBlockBody<P> {
         &self,
     ) -> &ContiguousList<ElectraAttesterSlashing<P>, P::MaxAttesterSlashingsElectra> {
         &self.attester_slashings
-    }
-
-    fn consolidation_requests_root(&self) -> H256 {
-        self.execution_requests.consolidations.hash_tree_root()
     }
 
     fn execution_requests(&self) -> &ExecutionRequests<P> {
@@ -1290,10 +1285,6 @@ impl<P: Preset> PostElectraBeaconBlockBody<P> for ElectraBlindedBeaconBlockBody<
         &self,
     ) -> &ContiguousList<ElectraAttesterSlashing<P>, P::MaxAttesterSlashingsElectra> {
         &self.attester_slashings
-    }
-
-    fn consolidation_requests_root(&self) -> H256 {
-        self.execution_requests.consolidations.hash_tree_root()
     }
 
     fn execution_requests(&self) -> &ExecutionRequests<P> {
