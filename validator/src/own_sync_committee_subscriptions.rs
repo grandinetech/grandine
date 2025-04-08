83d947f341ae1cf5f4d237342b6fc3137826318c
Tumas <tumas@grandine.io>
2025-04-08 17:11:09 +0300

    Use `BTreeMap::entry` when updating sync committee subscriptions, add test

--- validator/src/own_sync_committee_subscriptions.rs
+++ validator/src/own_sync_committee_subscriptions.rs
@@ -31,25 +31,23 @@ impl<P: Preset> OwnSyncCommitteeSubscriptions<P> {
         let next_period = current_period + 1;
         let next_period_start = misc::start_of_sync_committee_period::<P>(next_period);
 
-        if self.subscriptions.contains_key(&current_period) {
-            let subscriptions = core::iter::repeat(current_epoch)
+        self.subscriptions.entry(current_period).or_insert_with(|| {
+            core::iter::repeat(current_epoch)
                 .zip(sync_committee_subscriptions(
                     state,
                     own_public_keys,
                     state.current_sync_committee(),
                     next_period_start,
                 ))
-                .into_group_map();
+                .into_group_map()
+        });
 
-            self.subscriptions.insert(current_period, subscriptions);
-        }
-
-        if self.subscriptions.contains_key(&next_period) {
+        self.subscriptions.entry(next_period).or_insert_with(|| {
             let next_period_expiration = misc::start_of_sync_committee_period::<P>(next_period + 1);
 
             let mut rng = rand::thread_rng();
 
-            let subscriptions = sync_committee_subscriptions(
+            sync_committee_subscriptions(
                 state,
                 own_public_keys,
                 state.next_sync_committee(),
@@ -71,10 +69,8 @@ impl<P: Preset> OwnSyncCommitteeSubscriptions<P> {
 
                 (epoch_to_subscribe_at, subscription)
             })
-            .into_group_map();
-
-            self.subscriptions.insert(next_period, subscriptions);
-        }
+            .into_group_map()
+        });
     }
 
     pub fn discard_old_subscriptions(&mut self, current_epoch: Epoch) {
@@ -121,4 +117,79 @@ fn sync_committee_subscriptions<P: Preset>(
                 until_epoch,
             },
         )
+}
+
+#[cfg(test)]
+#[cfg(feature = "eth2-cache")]
+mod tests {
+    use eth2_cache_utils::holesky;
+    use hex_literal::hex;
+    use std_ext::ArcExt as _;
+    use types::{preset::Mainnet, traits::BeaconState as _};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_build_own_sync_committee_subscriptions() {
+        let state = holesky::CAPELLA_BEACON_STATE.force().clone_arc();
+        let mut own_sync_committee_subscriptions = OwnSyncCommitteeSubscriptions::default();
+
+        assert!(own_sync_committee_subscriptions.subscriptions.is_empty());
+
+        own_sync_committee_subscriptions.build(
+            state
+                .post_altair()
+                .expect("post-altair state must be present"),
+                &HashSet::from([
+                    // current sync committee member
+                    PublicKeyBytes::from(hex!("b8f0d05a9546ff830cd880170d4cb9c78aa0fd40d2b20919adc2ecb35ddac1496ade2819ff3b9d35244c0e0dc2b9dbe6")),
+                    // next sync committee member
+                    PublicKeyBytes::from(hex!("955bf6d459fca592c2eb0b71c934fcb046b40384e87870d87033013055cc698841c26d9e2f0e3c9e602ce319c630f8ec")),
+                    // non sync committee member
+                    PublicKeyBytes::from(hex!("83f66b69582ca67b8f0fc1e678015e72cce821d0930bcea582a011b495d6fed1e6c550b26d1a66331d8607ae34fd4423")),
+                ])
+        );
+
+        let state_slot = state.slot();
+        let current_epoch = misc::compute_epoch_at_slot::<Mainnet>(state_slot);
+        let current_sync_committee_period = misc::sync_committee_period::<Mainnet>(current_epoch);
+
+        let current_subscriptions = own_sync_committee_subscriptions
+            .subscriptions
+            .get(&current_sync_committee_period)
+            .and_then(|subscriptions| subscriptions.get(&current_epoch))
+            .expect("current sync committee subscriptions must have been built");
+
+        let next_committee_epoch = 1792;
+
+        assert_eq!(
+            *current_subscriptions,
+            vec![SyncCommitteeSubscription {
+                validator_index: 1_287_329,
+                sync_committee_indices: vec![509],
+                until_epoch: next_committee_epoch,
+            }],
+        );
+
+        let next_sync_committee_period =
+            misc::sync_committee_period::<Mainnet>(next_committee_epoch);
+
+        let next_subscriptions = own_sync_committee_subscriptions
+            .subscriptions
+            .get(&next_sync_committee_period)
+            .expect("next sync committee subscriptions must have been built");
+
+        assert_eq!(next_subscriptions.values().len(), 1);
+
+        for subscription in next_subscriptions.values() {
+            assert_eq!(
+                *subscription,
+                vec![SyncCommitteeSubscription {
+                    validator_index: 993_919,
+                    sync_committee_indices: vec![474],
+                    until_epoch: 2048,
+                }],
+            )
+        }
+    }
 }
