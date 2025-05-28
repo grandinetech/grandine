c2de11241e311024e621c05e23c083f0b297a423
hangleang <hangleang127@gmail.com>
2025-05-28 19:40:57 +0700

    Add `custody_group_count` to metadata in `node/identity` api response
    * Remove fulu from ignored globs spec tests

--- scripts/ci/consensus-spec-tests-coverage.rb
+++ scripts/ci/consensus-spec-tests-coverage.rb
@@ -20,7 +20,6 @@ IGNORED_GLOBS = %w[
   tests/*/eip7732/*/*/*/*/*.{ssz_snappy,yaml}
   tests/*/eip7441/*/*/*/*/*.{ssz_snappy,yaml}
   tests/*/eip7805/*/*/*/*/*.{ssz_snappy,yaml}
-  tests/*/fulu/*/*/*/*/*.{ssz_snappy,yaml}
   tests/diagnostics_obj.json{,.lock}
 ].map! { |glob| File.join(SUBMODULE, glob) }
 
