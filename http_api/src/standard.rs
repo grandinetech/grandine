cd598c8b3f32b1fe21676b75303a9c0b5f56a236
hangleang <hangleang127@gmail.com>
2025-06-11 10:09:22 +0700

    Use proofs from `engine_getPayloadV5` to construct cells_proofs

--- http_api/src/standard.rs
+++ http_api/src/standard.rs
@@ -1264,6 +1264,7 @@ pub async fn publish_block<P: Preset, W: Wait>(
     {
         let cells_and_kzg_proofs = eip_7594::try_convert_to_cells_and_kzg_proofs::<P>(
             blobs.as_ref(),
+            proofs.as_ref(),
             controller.store_config().kzg_backend,
         )?;
         let data_column_sidecars = eip_7594::construct_data_column_sidecars(
@@ -1333,6 +1334,7 @@ pub async fn publish_blinded_block<P: Preset, W: Wait>(
     {
         let cells_and_kzg_proofs = eip_7594::try_convert_to_cells_and_kzg_proofs::<P>(
             blobs.unwrap_or_default().as_ref(),
+            proofs.unwrap_or_default().as_ref(),
             controller.store_config().kzg_backend,
         )?;
         let data_column_sidecars = eip_7594::construct_data_column_sidecars(
@@ -1427,6 +1429,7 @@ pub async fn publish_block_v2<P: Preset, W: Wait>(
     {
         let cells_and_kzg_proofs = eip_7594::try_convert_to_cells_and_kzg_proofs::<P>(
             blobs.as_ref(),
+            proofs.as_ref(),
             controller.store_config().kzg_backend,
         )?;
         let data_column_sidecars = eip_7594::construct_data_column_sidecars(
