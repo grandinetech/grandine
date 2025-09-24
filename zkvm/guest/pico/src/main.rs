26a1d878a124b51b8cc7d249d3696e8f16ae9a22
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-09-24 13:58:13 +0800

    guest compiled

--- zkvm/guest/pico/src/main.rs
+++ zkvm/guest/pico/src/main.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use anyhow::Result;
-use pico_sdk::io::{commit, read_vec};
+use pico_sdk::io::{commit, read_vec, read_as};
 use pubkey_cache::PubkeyCache;
 use ssz::{SszRead as _, SszHash as _};
 use transition_functions::combined::untrusted_state_transition as state_transition;
@@ -15,54 +15,65 @@ use types::{
 pico_sdk::entrypoint!(main);
 
 pub fn main() {
-    // use Config::pectra_devnet_4() for Pectra devnet-4;
-    let config = Config::pectra_devnet_6();
+    println!("loading block and state...");
 
-    let (block, mut state, cache) = read_block_and_state::<Mainnet>(&config)
+    let (config, block, mut state, cache) = read_block_and_state::<Mainnet>()
         .expect("zkvm-guest-pico: read_block_and_state should succeed");
 
-    eprintln!("zkvm-guest-pico: state transition starts");
+    println!("loaded block and state");
+
+    println!("performing state transition...");
+
     state_transition(&config, &cache, &mut state, &block)
         .expect("zkvm-guest-pico: state_transtion should not fail");
-    eprintln!("zkvm-guest-pico: state transition ends");
+
+    println!("performed state transition");
 
     commit(&state.hash_tree_root().0);
+
+    println!("committed output");
 }
 
-fn read_block_and_state<P: Preset>(config: &Config) -> Result<(SignedBeaconBlock<P>, BeaconState<P>, PubkeyCache)> {
+fn read_block_and_state<P: Preset>() -> Result<(Config, SignedBeaconBlock<P>, BeaconState<P>, PubkeyCache)> {
+    let config_kind: u8 = read_as::<u8>();
+    let config = match config_kind {
+        0 => Config::mainnet(),
+        1 => Config::pectra_devnet_6(),
+        v => panic!("unknown config kind {v}"),
+    };
 
     let state_ssz = read_vec();
-    eprintln!("zkvm-guest-pico: state_ssz len: {}", state_ssz.len());
+    println!("state_ssz len: {}", state_ssz.len());
 
     let block_ssz = read_vec();
-    eprintln!("zkvm-guest-pico: block_ssz len: {}", block_ssz.len());
+    println!("block_ssz len: {}", block_ssz.len());
 
     let cache_ssz = read_vec();
-    eprintln!("zkvm-guest-pico: cache_ssz len: {}", cache_ssz.len());
+    println!("cache_ssz len: {}", cache_ssz.len());
 
     let phase_bytes = read_vec();
-    eprintln!("zkvm-guest-pico: phase_bytes len: {}", phase_bytes.len());
+    println!("phase_bytes len: {}", phase_bytes.len());
 
     let phase = enum_iterator::all::<Phase>()
         .zip(0_u8..)
         .find(|(_, index)| phase_bytes.get(0) == Some(&index))
         .map(|(phase, _)| phase);
 
-    eprintln!("zkvm-guest-pico: parsing SignedBeaconBlock starts");
+    println!("parsing SignedBeaconBlock starts");
     let block = match phase {
         Some(phase) => SignedBeaconBlock::<P>::from_ssz_at_phase(phase, &block_ssz)?,
-        None => SignedBeaconBlock::<P>::from_ssz(config, &block_ssz)?,
+        None => SignedBeaconBlock::<P>::from_ssz(&config, &block_ssz)?,
     };
-    eprintln!("zkvm-guest-pico: parsing SignedBeaconBlock ends");
+    println!("parsing SignedBeaconBlock ends");
 
-    eprintln!("zkvm-guest-pico: parsing BeaconState starts");
+    println!("parsing BeaconState starts: {:?}", phase);
     let state = match phase {
         Some(phase) => BeaconState::<P>::from_ssz_at_phase(phase, &state_ssz)?,
-        None => BeaconState::<P>::from_ssz(config, &state_ssz)?,
+        None => BeaconState::<P>::from_ssz(&config, &state_ssz)?,
     };
-    eprintln!("zkvm-guest-pico: parsing BeaconState ends");
+    println!("parsing BeaconState ends");
 
-    let cache = PubkeyCache::from_ssz(config, &cache_ssz)?;
+    let cache = PubkeyCache::from_ssz(&config, &cache_ssz)?;
 
-    Ok((block, state, cache))
+    Ok((config, block, state, cache))
 }
