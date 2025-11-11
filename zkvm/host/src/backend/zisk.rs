51504e2bc31f5419cf6bd16203c686d6e416d31a
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-11-11 21:38:16 +0800

    updated

--- zkvm/host/src/backend/zisk.rs
+++ zkvm/host/src/backend/zisk.rs
@@ -37,22 +37,9 @@ pub struct VMGuestInput {
 
 pub struct Vm;
 
-impl VmBackend for Vm {
-    type Report = Report;
-    type Proof = Proof;
-
-    fn new() -> Result<Self> {
-        Ok(Self)
-    }
-
-    fn execute(
-        &self,
-        config: ConfigKind,
-        state_ssz: Vec<u8>,
-        block_ssz: Vec<u8>,
-        cache_ssz: Vec<u8>,
-        phase_bytes: Vec<u8>,
-    ) -> Result<(Vec<u8>, Self::Report)> {
+impl Vm {
+    fn generate_input(&self, config: ConfigKind, state_ssz: Vec<u8>, block_ssz: Vec<u8>, cache_ssz: Vec<u8>, phase_bytes: Vec<u8>) -> Result<String>
+    {
         let serialized_data = bincode::serialize(&VMGuestInput {
             config: config as u8,
             state_ssz,
@@ -69,6 +56,27 @@ impl VmBackend for Vm {
         let input_path = output_dir.join("input.bin");
         std::fs::write(&input_path, &serialized_data)?;
 
+        Ok(input_path)
+    }
+}
+
+impl VmBackend for Vm {
+    type Report = Report;
+    type Proof = Proof;
+
+    fn new() -> Result<Self> {
+        Ok(Self)
+    }
+
+    fn execute(
+        &self,
+        config: ConfigKind,
+        state_ssz: Vec<u8>,
+        block_ssz: Vec<u8>,
+        cache_ssz: Vec<u8>,
+        phase_bytes: Vec<u8>,
+    ) -> Result<(Vec<u8>, Self::Report)> {
+        let input_path = self.generate_input(config, state_ssz, block_ssz, cache_ssz, phase_bytes)?;
         let zisk_guest_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("../guest/zisk");
 
         println!("Building the zkVM guest program");
@@ -82,10 +90,7 @@ impl VmBackend for Vm {
 
         if !build_output.status.success() {
             let stderr = String::from_utf8_lossy(&build_output.stderr);
-            return Err(anyhow::anyhow!(
-                "Failed to build zisk guest program. Stderr: {}",
-                stderr
-            ));
+            return Err(anyhow!("Failed to build zisk guest program. Stderr:\n{}", stderr));
         }
 
         println!("Executing the zkVM guest program with ziskemu");
@@ -93,7 +98,6 @@ impl VmBackend for Vm {
         // Second, execute the ELF file using ziskemu with a high step count.
         let elf_path =
             zisk_guest_dir.join("target/riscv64ima-zisk-zkvm-elf/release/zkvm_guest_zisk");
-        let input_path = zisk_guest_dir.join("build/input.bin");
 
         let output = Command::new("ziskemu")
             .env("RUST_BACKTRACE", "full")
@@ -158,6 +162,32 @@ impl VmBackend for Vm {
         _cache_ssz: Vec<u8>,
         _phase_bytes: Vec<u8>,
     ) -> Result<(Vec<u8>, Self::Proof)> {
+        // Refer to https://0xpolygonhermez.github.io/zisk/getting_started/writing_programs.html#prove
+        // 1. Run cmd for program setup
+        //   cargo-zisk rom-setup -e target/riscv64ima-zisk-zkvm-elf/release/zkvm_guest_zisk
+        // 2. Generate proof
+        //   LIB_EXT=$([[ "$(uname)" == "Darwin" ]] && echo "dylib" || echo "so")
+        //   cargo-zisk prove -e target/riscv64ima-zisk-zkvm-elf/release/zkvm_guest_zisk -i build/input.bin -o ./ -a
+        // 3. Verify proof
+        //   cargo-zisk verify -p ./proofs/vadcop_final_proof.bin
+        let input_path = self.generate_input(config, state_ssz, block_ssz, cache_ssz, phase_bytes)?;
+        let zisk_guest_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("../guest/zisk");
+        let zisk_elf_path = zisk_guest_dir
+
+        let setup_output = Command::new("cargo-zisk")
+            .arg("rom-setup")
+            .arg("-e")
+            .arg(zisk_elf_path)
+            .current_dir(&zisk_guest_dir)
+            .output()?;
+
+        if !build_output.status.success() {
+            let stderr = String::from_utf8_lossy(&build_output.stderr);
+            return Err(anyhow!("Failed to build zisk guest program. Stderr:\n{}", stderr));
+        }
+
+
+
         Ok((vec![], Proof))
     }
 }
