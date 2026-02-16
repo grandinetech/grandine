7769fa12d9b3cf30a8be0b3f9c7098f5491120ee
hangleang <hangleang127@gmail.com>
2026-02-16 21:49:49 +0700

    Add payload attestation tick

--- fork_choice_control/src/spec_tests.rs
+++ fork_choice_control/src/spec_tests.rs
@@ -175,7 +175,7 @@ async fn run_case<P: Preset>(config: &Arc<Config>, case: Case<'_>) {
     let pubkey_cache = Arc::new(PubkeyCache::default());
 
     let tick_at_time = |time| {
-        Tick::at_time(config, time, genesis_time)
+        Tick::at_time::<P>(config, time, genesis_time)
             .expect("configurations used in tests have valid values of SECONDS_PER_SLOT")
     };
 
