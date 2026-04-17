8d98403963e9e68d96662560c2307d44f3f937af
Povilas Liubauskas <povilas@grandine.io>
2026-04-17 14:40:42 +0300

    Gloas fork choice (implements v1.7.0-alpha.4 consensus specs)

--- scripts/ci/consensus-spec-tests-coverage.rb
+++ scripts/ci/consensus-spec-tests-coverage.rb
@@ -16,7 +16,6 @@ IGNORED_GLOBS = %w[
   tests/*/*/ssz_static/PartialDataColumnPartsMetadata/*/*/*.{ssz_snappy,yaml}
   tests/*/*/ssz_static/PartialDataColumnSidecar/*/*/*.{ssz_snappy,yaml}
   tests/*/heze/*/*/*/*/*.{ssz_snappy,yaml}
-  tests/*/gloas/fork_choice/*/*/*/*.{ssz_snappy,yaml}
   tests/general/phase0/ssz_generic/{compatible_unions,progressive_bitlist,progressive_containers,basic_progressive_list}/*/*/*.{ssz_snappy,yaml}
   tests/general/phase0/ssz_generic/containers/{valid,invalid}/{ProgressiveBitsStruct,ProgressiveTestStruct}_*/*.{ssz_snappy,yaml}
   tests/general/deneb/kzg/compute_challenge/*/*/*.{ssz_snappy,yaml}
