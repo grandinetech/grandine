dbd17eaa2f9f2b75e7cd2613dee50ecf7cbf2ec2
Povilas Liubauskas <povilas@grandine.io>
2026-04-23 19:47:48 +0300

    Added missing payload envelope validity checks

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -101,7 +101,7 @@ pub mod gloas {
     mod spec_tests;
 }
 
-pub use collections::{ProposerLookahead, Ptc, PtcWindow, Validators};
+pub use collections::{PayloadExpectedWithdrawals, ProposerLookahead, Ptc, PtcWindow, Validators};
 
 mod collections;
 
