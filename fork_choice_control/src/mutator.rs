1de3736a8b6eebf3e4e76602b727ac11c90a4064
hangleang <hangleang127@gmail.com>
2026-04-28 17:11:17 +0700

    Request payload from peer after delay attestation message

--- fork_choice_control/src/mutator.rs
+++ fork_choice_control/src/mutator.rs
@@ -1208,6 +1208,8 @@ where
                     .or_default()
                     .aggregates
                     .push(pending_aggregate_and_proof);
+
+                self.send_to_p2p(P2pMessage::PayloadEnvelopeNeeded(block_root, None));
             }
             Ok(AggregateAndProofAction::DelayUntilSlot(aggregate_and_proof)) => {
                 if let Some(metrics) = self.metrics.as_ref() {
@@ -1411,6 +1413,8 @@ where
                     .or_default()
                     .attestations
                     .push(attestation);
+
+                self.send_to_p2p(P2pMessage::PayloadEnvelopeNeeded(block_root, None));
             }
             Ok(AttestationAction::DelayUntilSlot(attestation)) => {
                 if let Some(metrics) = self.metrics.as_ref() {
