bebd8c4718f5b941e079f39a6b8a8ac53a638f84
sirse <artiom.tretjakovas2@gmail.com>
2026-04-29 13:06:17 +0300

    remove attestation rejection

--- fork_choice_store/src/store.rs
+++ fork_choice_store/src/store.rs
@@ -2478,9 +2478,11 @@ impl<P: Preset, S: Storage<P>> Store<P, S> {
 
             if index == 1 && !self.is_payload_verified(beacon_block_root) {
                 if is_from_block {
-                    bail!(Error::AttestationDataInvalidPayloadStatus {
-                        attestation: attestation.clone_arc()
-                    });
+                    // TODO(gloas): the block should be delayed in this place
+
+                    // bail!(Error::AttestationDataInvalidPayloadStatus {
+                    //     attestation: attestation.clone_arc()
+                    // });
                 }
 
                 return Ok(PartialAttestationAction::DelayUntilPayload(
