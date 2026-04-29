1a54657bd298c0dc8f7b4a9bef5e1ad3b954a0d5
Povilas Liubauskas <povilas@grandine.io>
2026-04-29 16:16:36 +0200

    Test

--- operation_pools/src/attestation_agg_pool/tasks.rs
+++ operation_pools/src/attestation_agg_pool/tasks.rs
@@ -19,6 +19,7 @@ use tap::Pipe as _;
 use types::{
     combined::{Attestation as CombinedAttestation, BeaconState},
     electra::error::AttestationConversionError,
+    nonstandard::Phase,
     phase0::{
         containers::Attestation,
         primitives::{CommitteeIndex, Epoch, H256, Slot, ValidatorIndex},
@@ -225,6 +226,13 @@ impl<P: Preset, W: Wait> PoolTask for InsertAttestationTask<P, W> {
 
         if let CombinedAttestation::Single(single_attestation) = attestation.as_ref() {
             attester_index = Some(single_attestation.attester_index);
+        }
+
+        // TODO(Gloas): remove after testing
+        if controller.phase() >= Phase::Gloas {
+            if attestation.data().index != 1 {
+                return Ok(());
+            }
         }
 
         let attestation = match convert_attestation_for_pool(&controller, attestation) {
