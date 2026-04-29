bb608fc1a1dd5d978536ec9283ed055e6509b0ee
sirse <artiom.tretjakovas2@gmail.com>
2026-04-29 18:04:29 +0300

    block production testing

--- block_producer/src/block_producer.rs
+++ block_producer/src/block_producer.rs
@@ -1045,7 +1045,7 @@ impl<P: Preset, W: Wait> BlockBuildContext<P, W> {
                                 graffiti,
                                 proposer_slashings,
                                 attester_slashings: self.prepare_attester_slashings_electra().await,
-                                attestations,
+                                attestations: Default::default(),
                                 deposits,
                                 voluntary_exits,
                                 sync_aggregate,
