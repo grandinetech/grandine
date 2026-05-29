ee722171cc06c7a3989de6f3661cd7c9c918dd8e
Povilas Liubauskas <povilas@grandine.io>
2026-05-29 11:03:41 +0300

    Code style fixes

--- fork_choice_control/src/queries.rs
+++ fork_choice_control/src/queries.rs
@@ -1001,7 +1001,7 @@ where
         origin: &ExecutionPayloadEnvelopeOrigin,
         block_info: impl FnOnce() -> Option<(Arc<SignedBeaconBlock<P>>, PayloadStatus)>,
         state_fn: impl FnOnce() -> Option<Arc<BeaconState<P>>>,
-        execution_engine: impl ExecutionEngine<P> + Send,
+        execution_engine: &(impl ExecutionEngine<P> + Send),
     ) -> Result<ExecutionPayloadEnvelopeAction<P>> {
         self.store_snapshot()
             .validate_execution_payload_envelope_with_state(
