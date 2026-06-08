d16f86f3d6083d3d236db65439d34ce0841cea96
Artiom Tretjakovas <artiom@grandine.io>
2026-06-08 13:55:30 +0300

    Implemented engine_getPayloadV6 for C# plugin

--- bindings/csharp/Grandine.NethermindPlugin/GrandineUtils.cs
+++ bindings/csharp/Grandine.NethermindPlugin/GrandineUtils.cs
@@ -291,7 +291,9 @@ public static class GrandineUtils
             Withdrawals = WithdrawalsFromNative(attr.value.withdrawals),
             ParentBeaconBlockRoot = attr.value.parent_beacon_block_root.ToHash256(),
             SlotNumber = attr.value.slot_number,
-            TargetGasLimit = attr.value.target_gas_limit,
+
+            // TODO: The target gas limit must be passed, but nethermind currently doesn't have field for it.
+            // TargetGasLimit = attr.value.target_gas_limit,
         };
     }
 
