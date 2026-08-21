0044368736a8b10180526c8f2021dcd22ef407cd
Povilas Liubauskas <povilas@grandine.io>
2026-08-21 12:19:28 +0300

    Updated SSZ spec runner

--- scripts/ci/consensus-spec-tests-coverage.rb
+++ scripts/ci/consensus-spec-tests-coverage.rb
@@ -4,9 +4,9 @@
 # If a glob in the codebase matches the path of a test case directory,
 # every file in that test case is assumed to be covered.
 
-DIRECTORY = 'consensus-spec-tests'
+CONSENSUS_SPEC_TESTS = 'consensus-spec-tests'
 
-IGNORED_GLOBS = %w[
+CONSENSUS_IGNORED_GLOBS = %w[
   .version
   **/*.md
   **/*.py
@@ -23,42 +23,71 @@ IGNORED_GLOBS = %w[
   tests/*/*/ssz_static/PartialDataColumnSidecar/*/*/*.{ssz_snappy,yaml}
   tests/*/heze/*/*/*/*/*.{ssz_snappy,yaml}
   tests/*/*/fast_confirmation/*/*/*/*.{ssz_snappy,yaml}
-  tests/general/phase0/ssz_generic/compatible_unions/*/*/*.{ssz_snappy,yaml}
   tests/*/*/networking/*/pyspec_tests/*/*.{ssz_snappy,yaml}
-].map! { |glob| File.join(DIRECTORY, glob) }
+].map! { |glob| File.join(CONSENSUS_SPEC_TESTS, glob) }
 
-LS_FILES_COMMAND = ['find', DIRECTORY, '-type', 'f', '-print0']
+SSZ_SPEC_TESTS = 'ssz-spec-tests'
 
-GREP_COMMAND = %W[
-  grep
-  -ErohI
-  #{DIRECTORY}/tests/[^\"`]+
-]
+SSZ_IGNORED_GLOBS = %w[
+  .version
+].map! { |glob| File.join(SSZ_SPEC_TESTS, glob) }
 
 Dir.chdir(File.join(__dir__, '..', '..'))
 
-all_files = IO.popen(LS_FILES_COMMAND, IO::RDONLY | IO::BINARY) do |io|
+success = true
+
+# check consensus spec coverage
+
+all_files = IO.popen(['find', CONSENSUS_SPEC_TESTS, '-type', 'f', '-print0'], IO::RDONLY | IO::BINARY) do |io|
   io.each_line("\0").map { |line| line.delete_suffix("\0") }
 end
 
-covered_globs = IO.popen(GREP_COMMAND) do |io|
+covered_globs = IO.popen(%W[grep -ErohI #{CONSENSUS_SPEC_TESTS}/tests/[^\"`]+]) do |io|
   io.each_line.map do |line|
     File.join(line.chomp!, '*.{ssz_snappy,yaml}')
   end
 end
 
-covered_files = Dir.glob(covered_globs + IGNORED_GLOBS)
+covered_files = Dir.glob(covered_globs + CONSENSUS_IGNORED_GLOBS)
 uncovered_files = all_files - covered_files
 
 if uncovered_files.empty?
-  puts("All #{all_files.size} files in #{DIRECTORY} are covered by tests.")
+  puts("All #{all_files.size} files in #{CONSENSUS_SPEC_TESTS} are covered by tests.")
 else
   heading = <<~END
     #{uncovered_files.size} of #{all_files.size} \
-    files in #{DIRECTORY} are not covered by tests:
+    files in #{CONSENSUS_SPEC_TESTS} are not covered by tests:
   END
 
   puts(heading, uncovered_files)
 
-  exit(false)
+  success = false
 end
+
+# check ssz spec coverage
+
+all_files = IO.popen(['find', SSZ_SPEC_TESTS, '-type', 'f', '-print0'], IO::RDONLY | IO::BINARY) do |io|
+  io.each_line("\0").map { |line| line.delete_suffix("\0") }
+end
+
+covered_globs = IO.popen(%W[grep -ErohI #{SSZ_SPEC_TESTS}/fixtures/[^\"`]+\\.json]) do |io|
+  io.each_line.map { |line| line.chomp }
+end
+
+covered_files = Dir.glob(covered_globs + SSZ_IGNORED_GLOBS)
+uncovered_files = all_files - covered_files
+
+if uncovered_files.empty?
+  puts("All #{all_files.size} files in #{SSZ_SPEC_TESTS} are covered by tests.")
+else
+  heading = <<~END
+    #{uncovered_files.size} of #{all_files.size} \
+    files in #{SSZ_SPEC_TESTS} are not covered by tests:
+  END
+
+  puts(heading, uncovered_files)
+
+  success = false
+end
+
+exit(success)
