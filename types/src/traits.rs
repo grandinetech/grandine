affa59263e49a83fe6ebf15b2c5356a764993a33
Artiom Tretjakovas <artiom@grandine.io>
2026-08-26 16:08:28 +0300

    Expose historical summaries through beacon state traits

--- types/src/traits.rs
+++ types/src/traits.rs
@@ -60,8 +60,8 @@ use crate::{
     },
     collections::{
         BuilderPendingPayments, BuilderPendingWithdrawals, Builders, Eth1DataVotes,
-        HistoricalRoots, PayloadExpectedWithdrawals, ProposerLookahead, PtcWindow, RandaoMixes,
-        RecentRoots, Slashings,
+        HistoricalRoots, HistoricalSummaries, PayloadExpectedWithdrawals, ProposerLookahead,
+        PtcWindow, RandaoMixes, RecentRoots, Slashings,
     },
     combined::{
         Attestation as CombinedAtteststation, AttesterSlashing as CombinedAttesterSlashing,
@@ -709,6 +709,9 @@ pub trait PostCapellaBeaconState<P: Preset>: PostBellatrixBeaconState<P> {
 
     fn next_withdrawal_validator_index(&self) -> ValidatorIndex;
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex;
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P>;
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P>;
 }
 
 impl<P: Preset, S: PostCapellaBeaconState<P>> PostCapellaBeaconState<P> for Hc<S> {
@@ -726,6 +729,14 @@ impl<P: Preset, S: PostCapellaBeaconState<P>> PostCapellaBeaconState<P> for Hc<S
 
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex {
         self.as_mut().next_withdrawal_validator_index_mut()
+    }
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P> {
+        self.as_ref().historical_summaries()
+    }
+
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P> {
+        self.as_mut().historical_summaries_mut()
     }
 }
 
@@ -745,6 +756,14 @@ impl<P: Preset> PostCapellaBeaconState<P> for CapellaBeaconState<P> {
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex {
         &mut self.next_withdrawal_validator_index
     }
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P> {
+        &self.historical_summaries
+    }
+
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P> {
+        &mut self.historical_summaries
+    }
 }
 
 impl<P: Preset> PostCapellaBeaconState<P> for DenebBeaconState<P> {
@@ -762,6 +781,14 @@ impl<P: Preset> PostCapellaBeaconState<P> for DenebBeaconState<P> {
 
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex {
         &mut self.next_withdrawal_validator_index
+    }
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P> {
+        &self.historical_summaries
+    }
+
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P> {
+        &mut self.historical_summaries
     }
 }
 
@@ -781,6 +808,14 @@ impl<P: Preset> PostCapellaBeaconState<P> for ElectraBeaconState<P> {
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex {
         &mut self.next_withdrawal_validator_index
     }
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P> {
+        &self.historical_summaries
+    }
+
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P> {
+        &mut self.historical_summaries
+    }
 }
 
 impl<P: Preset> PostCapellaBeaconState<P> for FuluBeaconState<P> {
@@ -799,6 +834,14 @@ impl<P: Preset> PostCapellaBeaconState<P> for FuluBeaconState<P> {
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex {
         &mut self.next_withdrawal_validator_index
     }
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P> {
+        &self.historical_summaries
+    }
+
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P> {
+        &mut self.historical_summaries
+    }
 }
 
 impl<P: Preset> PostCapellaBeaconState<P> for GloasBeaconState<P> {
@@ -816,6 +859,14 @@ impl<P: Preset> PostCapellaBeaconState<P> for GloasBeaconState<P> {
 
     fn next_withdrawal_validator_index_mut(&mut self) -> &mut ValidatorIndex {
         &mut self.next_withdrawal_validator_index
+    }
+
+    fn historical_summaries(&self) -> &HistoricalSummaries<P> {
+        &self.historical_summaries
+    }
+
+    fn historical_summaries_mut(&mut self) -> &mut HistoricalSummaries<P> {
+        &mut self.historical_summaries
     }
 }
 
